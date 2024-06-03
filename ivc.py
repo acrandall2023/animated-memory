4d3e6490a8b9a482659adc4b45dccb805b14633f
acrandall2023 <122498320+acrandall2023@users.noreply.github.com>
2024-06-03 16:17:04 -0700

    Add files via upload

--- ivc.py
+++ ivc.py
@@ -1,5 +1,6 @@
 """
-This script is intended to pull information from JIRA about IVC related tickets to export to google sheets
+This script is intended to pull information from JIRA about
+IVC related tickets to export to google sheets
 """
 
 # Keyring is used for JIRA authentication
@@ -17,6 +18,13 @@ from atlassian import Jira
 # For timing decorator
 from time import time
 
+# os is used to get the directory for the credentials for pygsheets
+import os
+
+# makes prints prettier
+from rich import print
+
+
 # Constants to replace customfields in the export
 ADDRESS = "customfield_11520"
 ITEMS = "customfield_11516"
@@ -26,30 +34,20 @@ INSURANCE = "customfield_11506"
 SITE_NAME = "customfield_11507"
 TRK_NUM = "customfield_11509"
 ARRIVAL = "customfield_11505"
+DELSPEED = "customfield_11600"
+JIRAURL = "https://servicedesk.cenic.org/"
+# path for credentials for pygsheets
+GCPATH = os.path.expanduser("~") + "/Documents/Python"
 
 # using keyring to log in to JIRA
 jira = Jira(
-    url="https://servicedesk.cenic.org/",
+    JIRAURL,
     username=keyring.get_password("cas", "user"),
     password=keyring.get_password("cas", "password"),
 )
 
 
-def timer_func(func):
-    # This function shows the execution time of
-    # the function object passed
-    def wrap_func(*args, **kwargs):
-        t1 = time()
-        result = func(*args, **kwargs)
-        t2 = time()
-        print(f"Function {func.__name__!r} executed in {(t2-t1):.4f}s")
-        return result
-
-    return wrap_func
-
-
 class IVCJira:
-    @timer_func
     def get_ship():
         # The query to find open tickets
         _jql = 'project = "Inventory Control" AND issuetype = "Outbound Shipping" AND status not in (Deleted, New) AND updated > endOfDay(-4) ORDER BY createdDate ASC'
@@ -65,51 +63,33 @@ class IVCJira:
             ivc += queryJira["issues"][i]["key"].split()
         return ivc
 
-    @timer_func
-    def get_req():
-        _jql = 'project = "Inventory Control" AND issuetype = "Equipment Request" AND status not in (Deleted, New) AND updated > endOfDay(-4) ORDER BY createdDate asc'
-        queryJira = jira.jql(_jql)
-        ivc = []
-        for i in range(len(queryJira["issues"])):
-            ivc += queryJira["issues"][i]["key"].split()
-        return ivc
-
-    @timer_func
-    def get_ret():
-        _jql = 'project = "Inventory Control" AND issuetype = "Equipment Returns" AND status not in (Deleted, New) AND updated > endOfDay(-4) ORDER BY createdDate asc'
-        queryJira = jira.jql(_jql)
-        ivc = []
-        for i in range(len(queryJira["issues"])):
-            ivc += queryJira["issues"][i]["key"].split()
-        return ivc
-
-    @timer_func
     def get_ivc(ivc):
         # establish ivc_info as a list
         ivc_info = []
+        shipinfo = []
 
-        # contacting jira to get information about ivc ticket provided from ivc.txt
+        # contacting jira to get information about ivc ticket provided by for loop
         results = {}
         try:
             results = jira.issue(ivc)
-        except:
+        except Exception:
             ivc_info.append(ivc)
 
         # interpret results into individual variables, if the ticket doesn't exist, go to exception
         try:
             ticket = results["key"]
-            issue = results["fields"]["issuetype"]["name"]
             created = parser.isoparse(results["fields"]["created"]).strftime("%m-%d-%Y")
             status = results["fields"]["status"]["name"]
             requestor = results["fields"]["reporter"]["displayName"]
-            # contact = results["fields"][SITE_CONTACT]
-            # phone = results["fields"][PHONE]
             requestItems = results["fields"][ITEMS]
-            # insurance = str(results["fields"][INSURANCE])
-            siteName = results["fields"][SITE_NAME]
+            siteName = results["fields"][SITE_NAME].replace(" ", "_").replace(".", "")
             tracking = results["fields"][TRK_NUM]
-            arrivalDate = results["fields"][ARRIVAL]
-        except:
+            if (results["fields"][DELSPEED]) is None:
+                arrivalDate = results["fields"][ARRIVAL]
+            else:
+                arrivalDate = results["fields"][DELSPEED]["value"]
+
+        except Exception:
             ticket = "No ticket"
 
         # append all information to ivc_info, if the results are an empty dictionary, append no result
@@ -118,61 +98,68 @@ class IVCJira:
             ivc_info.append("No result")
         else:
             ivc_info.append(ticket)
-            ivc_info.append(issue)
+            ivc_info.append(siteName)
+            ivc_info.append(requestItems)
             ivc_info.append(created)
             ivc_info.append(requestor)
-            ivc_info.append(requestItems)
-            ivc_info.append(siteName)
-            # ivc_info.append(contact)
-            # ivc_info.append(phone)
-            # ivc_info.append(insurance)
             ivc_info.append(arrivalDate)
             ivc_info.append(tracking)
             ivc_info.append(status)
-        return ivc_info
+            shipinfo.append(ticket)
+            shipinfo.append(tracking)
+            shipinfo.append(siteName)
+        return ivc_info, shipinfo
 
 
-@timer_func
 def main():
+    totalT1 = time()
     ivcship = IVCJira.get_ship()
-    ivcreq = IVCJira.get_req()
-    ivcret = IVCJira.get_ret()
 
-    # Authenticate with GSheets using client_secret.json in current directory
-    gc = pygsheets.authorize(local=True)
+    """
+    Authenticate with GSheets using client_secret.json in ~/Documents/Python
+    After authenticating, pygsheets will create a file named "sheets.googleapis.com-python.json" in ~/Documents/Python
+    If an Unauthorized error occurs, delete "sheets.googleapis.com-python.json" and run this script again to re-authenticate
+    """
+
+    gc = pygsheets.authorize(credentials_directory=GCPATH, local=True)
 
     # Open the sheet to edit
     sh = gc.open("IVC Tracker")
 
     # Which sheet of the whole worksheet to edit
     wkship = sh.worksheet("title", "OutboundShipping")
-    wkreq = sh.worksheet("title", "EquipmentRequest")
-    wkret = sh.worksheet("title", "EquipmentReturns")
+    shipdate = sh.worksheet("title", "ShipDate")
 
     # for each item in ivcship
+    print(f"[green]Total number of tickets to check:[/green] {len(ivcship)}")
     for i in ivcship:
         # Check if the value already exists, if it does update it
         if wkship.find(i, matchCase=True) != []:
+            t1 = time()
             existRow = wkship.find(i, matchCase=True)[0].row
-            wkship.update_row(existRow, IVCJira.get_ivc(i))
+            wkship.update_row(existRow, IVCJira.get_ivc(i)[0])
+            t2 = time()
+            print(
+                f"[green]ivcship[/green] for [red]{i}[/red] executed in {(t2-t1):.4f}s"
+            )
+
         # If it does not exist already, create new row and add information
         else:
-            wkship.append_table(IVCJira.get_ivc(i), overwrite=False)
+            t1 = time()
+            wkship.append_table(IVCJira.get_ivc(i)[0], overwrite=False)
+            t2 = time()
+            print(
+                f"[green]ivcship[/green] for [red]{i}[/red] executed in {(t2-t1):.4f}s - New line added"
+            )
 
-    for i in ivcreq:
-        if wkreq.find(i, matchCase=True) != []:
-            existRow = wkreq.find(i, matchCase=True)[0].row
-            wkreq.update_row(existRow, IVCJira.get_ivc(i))
+        if shipdate.find(i, matchCase=True) != []:
+            existShip = shipdate.find(i, matchCase=True)[0].row
+            shipdate.update_row(existShip, IVCJira.get_ivc(i)[1])
         else:
-            wkreq.append_table(IVCJira.get_ivc(i), overwrite=False)
+            shipdate.append_table(IVCJira.get_ivc(i)[1], overwrite=False)
 
-    for i in ivcret:
-        if wkret.find(i, matchCase=True) != []:
-            existRow = wkret.find(i, matchCase=True)[0].row
-            wkret.update_row(existRow, IVCJira.get_ivc(i))
-        else:
-            wkret.append_table(IVCJira.get_ivc(i), overwrite=False)
-    print("Done. IVC Tracker has been updated with latest information")
+    totalT2 = time()
+    print(f"[blue]Total Time taken is[/blue] [cyan]{(totalT2-totalT1):.4f}s[/cyan]")
 
 
 if __name__ == "__main__":
--- parc.py
+++ parc.py
@@ -2,29 +2,36 @@
 information retrieval script testing
 """
 
-import csv
 import requests
 import keyring
 import pygsheets
 from dateutil import parser
 from bs4 import BeautifulSoup
 from atlassian import Jira
+import os
+from time import time
+from rich import print
+import urllib3
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 APPROVERS = "customfield_10100"
+JIRAURL = "https://servicedesk.cenic.org/"
+PARCURL = (
+    "https://parc.cenic.org/cenic-parc/printpurchaseorder.cfm?purchaseordersnumber="
+)
+GCPATH = os.path.expanduser("~") + "/Documents/Python"
 
 
 class PARC:
-    url = (
-        "https://parc.cenic.org/cenic-parc/printpurchaseorder.cfm?purchaseordersnumber="
-    )
+    url = PARCURL
 
     def __init__(self):
         self.soup = None
         self.po_elem = None
 
     def _get_jira(self) -> str:
-        self.jira = self.soup.find(text="RT NUMBER:")
+        self.jira = self.soup.find(string="RT NUMBER:")
         jiraticket = self.jira.next_element.next_element.text.strip("\r\n         ")
         if jiraticket:
             if int(jiraticket) > 10000:
@@ -37,7 +44,7 @@ class PARC:
         jiraticket = self._get_jira()
         if "PUR" in jiraticket:
             jiraQuery = Jira(
-                url="https://servicedesk.cenic.org/",
+                JIRAURL,
                 username=keyring.get_password("cas", "user"),
                 password=keyring.get_password("cas", "password"),
             )
@@ -48,7 +55,7 @@ class PARC:
                 approval1 = parser.isoparse(
                     (results["fields"][APPROVERS][0]["completedDate"]["iso8601"])
                 ).strftime("%m-%d-%Y")
-            except:
+            except Exception:
                 approval1 = ""
 
             try:
@@ -56,7 +63,7 @@ class PARC:
                 approval2 = parser.isoparse(
                     (results["fields"][APPROVERS][1]["completedDate"]["iso8601"])
                 ).strftime("%m-%d-%Y")
-            except:
+            except Exception:
                 approval2 = ""
 
             try:
@@ -64,29 +71,29 @@ class PARC:
                 approval3 = parser.isoparse(
                     (results["fields"][APPROVERS][2]["completedDate"]["iso8601"])
                 ).strftime("%m-%d-%Y")
-            except:
+            except Exception:
                 approval3 = ""
 
             return [approval1, approval2, approval3]
         else:
-            return ["", "", ""]
+            return ["", "NA", ""]
 
     def _get_ordered(self) -> str:
-        self.order = self.soup.find(text="Requested By")
+        self.order = self.soup.find(string="Requested By")
         ordered = self.order.next_element
         if ordered:
             return ordered.text.strip("PUR-1234567890").strip(": ")
         return "Ordered By Not Found"
 
     def _get_segment(self) -> str:
-        self.seg = self.soup.find(text="Goods/Services for:")
+        self.seg = self.soup.find(string="Goods/Services for:")
         segment = self.seg.next_element
         if segment:
             return segment.text.strip(" ")
         return "Segment Not Found"
 
     def _get_vendor(self) -> str:
-        self.vend = self.soup.find(text="Vendor Information:")
+        self.vend = self.soup.find(string="Vendor Information:")
         vendor = self.vend.next_element.next_element
         if vendor:
             return vendor.text.strip("\r\n         ")
@@ -102,7 +109,7 @@ class PARC:
         return "Grand Total Not Found"
 
     def _get_date(self) -> str:
-        self.date = self.soup.find(text="DATE:")
+        self.date = self.soup.find(string="DATE:")
         podate = self.date.next_element.next_element
         if podate:
             return podate.text.strip("\r\n         ")
@@ -117,13 +124,12 @@ class PARC:
         segment: bool = True,
         vendor: bool = True,
         grand_total: bool = True,
-        status: bool = True,
         approval: bool = True,
     ) -> list:
         # Return list of data for given PO Number
         po_info = [po]
 
-        resp = requests.get(self.url + po)
+        resp = requests.get(self.url + po, verify=False)
         self.soup = BeautifulSoup(resp.text, "html.parser")
 
         if podate:
@@ -143,33 +149,37 @@ class PARC:
             po_info.append("")
             po_info.append("")
             po_info.append("")
-            po_info.append("")
-            po_info.append("")
             po_info.append(self._get_approval()[2])
             po_info.append(self._get_approval()[1])
             po_info.append(self._get_approval()[0])
-
         return po_info
 
 
 def main():
+    t1 = time()
     parc = PARC()
 
-    gcpath = "/Users/acrandall/Documents/Python/OAuth/client_secret.json"
-    gc = pygsheets.authorize(client_secret=gcpath, local=True)
+    # Credentials are assumed to be stored in the same directory and named client_secret.json
+    print("[green]Authorizing with GSheets")
+    gc = pygsheets.authorize(credentials_directory=GCPATH, local=True)
+    print("[green]Selecting New Orders spreadsheet")
     sh = gc.open("Inventory: New Orders Tracking")
     wk1 = sh.worksheet("index", "0")
 
     findPos = wk1.get_col(1, include_tailing_empty=False)
     po = str(int(findPos[-1]) + 1)
+    print(f"Starting PO is {po}")
 
     while parc.get_po_info(po)[2] != "PO Does not exist":
         poNum = str(int(po) - 1)
+        print(f"poNum is {poNum}")
         noRow = int(wk1.find(poNum, matchCase=True, matchEntireCell=True)[0].row) + 1
         wk1.update_row(noRow, parc.get_po_info(po))
         po = str(int(po) + 1)
     else:
-        print("End")
+        t2 = time()
+        print(f"[blue]Total Time taken is[/blue] [cyan]{(t2-t1):.4f}s[/cyan]")
+        print("[green]New Orders spreadsheet updated to current[/green]")
 
 
 if __name__ == "__main__":
